1e27188517f9e3c04bb80468f1a53adc8130f1e6
Jose Vargas <josemvargas94@gmail.com>
2014-08-19 13:48:24 -0400

    Added more content to about page

--- about.php
+++ about.php
@@ -14,9 +14,33 @@
      	<div id="aboutInfo" class="center-block col-md-9">
       	<div >
       		<h4>About</h4>
-					<p>To submit new projects, please fill out this <a href="add_project.php" >form</a> (last updated: 2014 Aug 18)</p>
-				</div>
-			</div>
+      		<p class = "overview">As an organization, UNICEF has been innovating for decades. Over the past
+	 		few months, we invited country offices to input their innovative activities
+ 			in to the mapping and see their initiatives along with others in
+ 			realtime. Based on their input, we could identify trends, commonalities,
+ 			gaps and difficulties, and highlight successes.</p>
+
+ 			<p class = "overview"> We are dedicated to improve the map, and make it more useful for our
+ 			colleagues around the world, external partners as well as anyone who might
+ 			be interested. In the second iteration of the map, some of the added
+ 			features and changes are as follows:</p><br>
+ 			<ul>
+    			<li>User logins that allow updates and edits to your submitted projects</li>
+    			<li>Search and filter ability</li>
+    			<li>Updated questionnaire</li>
+    			<li>Allowing for embedded multimedia content</li>
+    			<li>Improved visuals.</li>
+			</ul>
+			<br>
+ 			<p class = "overview">We continue to welcome your input into the map and check out other
+ 			innovation initiatives. Please note that each UNICEF country office and
+ 			regional office is provided with one login, which is required to submit and
+ 			edit projects.</p>
+ 			If you have any question, please feel free to contact the Innovation
+ 			Unit.</p>
+			<p>To submit new projects, please fill out <a href="add_project.php" >this form</a> (last updated: 2014 Aug 18)</p>
+		</div>
+		</div>
 		</div>
 	</div>
 	<?php jsDocuments(); ?>
